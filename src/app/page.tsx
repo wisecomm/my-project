2644afb0ac7fd3dab3f9be0e6be4000c177df449
danyoh <danyoh@gmail.com>
2025-02-13 17:32:48 +0900

    레이아웃 테스트 - tailwind 공식버전 나오면 샛팅하자

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div className="bg-red-500 p-12">
-      <h1 className="text-5xl">
-        Welcome to
-      </h1>
+      <h1 className="text-5xl">Welcome to</h1>
       <Button>shadcn button</Button>
+      <div className="flex flex-1 items-center justify-end">
+        <ModeToggle />
       </div>
+    </div>
   );
 }
